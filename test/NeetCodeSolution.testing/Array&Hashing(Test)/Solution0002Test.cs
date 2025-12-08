5650b4fc74ec99df9dfaf25769889106a1ae81f8
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-12-09 04:40:32 +0500

    Add unit tests for Solution0002's IsAnagram method
    Introduced a new test class `Solution0002Test` under the
    `NeetCodeSolution.testing.Array_Hashing_Test_` namespace to validate
    the `IsAnagram` method in `Solution0002`.
    
    - Added a `using` directive for the required namespace.
    - Implemented `IsAnagrams` test to verify correct anagram detection.
    - Implemented `IsNotAnagrams` test to verify non-anagram detection
      (note: assertion in this test may need revision).

---
+++ test/NeetCodeSolution.testing/Array&Hashing(Test)/Solution0002Test.cs
@@ -0,0 +1,26 @@
+﻿using NeetCodeSolution.ProblemSolution.Array_Hashing;
+
+namespace NeetCodeSolution.testing.Array_Hashing_Test_;
+
+public class Solution0002Test
+{
+    private readonly Solution0002 _solution = new Solution0002();
+
+    [Fact]
+    public void IsAnagrams()
+    {
+        string s = "listen" , t = "netils";
+
+        bool result = _solution.IsAnagram(s, t);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotAnagrams()
+    {
+        string s = "hello", t = "world";
+        bool result = _solution.IsAnagram(s, t);
+        Assert.True(result);
+    }
+}
